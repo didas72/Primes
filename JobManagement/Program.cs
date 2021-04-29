c9ac53a0af70eff2be654f0ac9aa2e7686f590d8
Didas72 <diogocruzdiniz@gmail.com>
2021-04-29 16:57:02 +0100

    Added .gitignore

---
+++ JobManagement/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Primes;
+using Primes.Common;
+using Primes.Common.Files;
+
+namespace JobManagement
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Here goes code that will only get executed a few times for testing purpose and will never be used again.
+            //Please ignore this project.
+        }
+    }
+}
