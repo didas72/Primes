582bf9abbb3a99987f1c3e45c04cc064fdd60326
Didas72 <diogocruzdiniz@gmail.com>
2021-04-30 19:57:41 +0100

    Added XML comments to Primes.Common.ExtensionMethods

--- Primes.Common/ExtensionMethods.cs
+++ Primes.Common/ExtensionMethods.cs
@@ -1,14 +1,32 @@
-﻿namespace Primes.Common
+﻿using System;
+
+namespace Primes.Common
 {
+    /// <summary>
+    /// Class that contains several extension methods.
+    /// </summary>
     public static class ExtensionMethods
     {
+        /// <summary>
+        /// Truncates the string to a given length.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="maxLength">The length to truncate the string to.</param>
+        /// <returns>Truncated string.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
         public static string Truncate(this string value, int maxLength)
         {
             if (string.IsNullOrEmpty(value))
                 return value;
             return value.Length <= maxLength ? value : value.Substring(0, maxLength);
         }
-
+        /// <summary>
+        /// Returns a string with the given length, either truncated or padded with spaces.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="length">The length to set the string to.</param>
+        /// <returns>Rescaled string.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
         public static string SetLength(this string value, int length)
         {
             if (value.Length >= length)
@@ -16,7 +34,12 @@
 
             return value += " ".Loop(length - value.Length);
         }
-
+        /// <summary>
+        /// Repeats the string a given amount of times.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="times">The number of times to repeat the string.</param>
+        /// <returns>Looped string.</returns>
         public static string Loop(this string value, int times)
         {
             string f = string.Empty;
