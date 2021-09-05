81bf435f1e43c1a85ee5b65c4d8258d6c4931b00
Didas72 <diogocruzdiniz@gmail.com>
2021-09-05 18:16:39 +0100

    Removed PrimesCpp

--- PrimesCpp/ConsoleIO.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <iostream>
-
-#include "ShortTypeNames.h"
-
-void Printf(std::string message);
-
-std::string ReadLine();
--- PrimesCpp/PrimesCpp.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <iostream>
-
-#include "ShortTypeNames.h"
-
-ulong GetUlongInput();
