d8c2777c4b66503e30dbd4642dde79b3eb190f55
Didas72 <diogocruzdiniz@gmail.com>
2021-09-06 14:01:55 +0100

    Started implementing code testing

---
+++ UnitTestsPrimes/PrimesTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Primes.Common;
+
+namespace UnitTestsPrimes
+{
+    [TestClass]
+    public class PrimesTests
+    {
+        [TestMethod]
+        [DataRow((ulong)2)]
+        [DataRow((ulong)11)]
+        [DataRow((ulong)1203117401197)]
+        public void PrimesSimple_PrimesNumbers_ReturnTrue(ulong value)
+        {
+            //arrange
+            bool isPrime;
+
+            //act
+            isPrime = Mathf.IsPrime(value);
+
+            //assert
+            Assert.IsTrue(isPrime);
+        }
+
+        [TestMethod]
+        [DataRow((ulong)0)]
+        [DataRow((ulong)1)]
+        [DataRow((ulong)1203117401198)]
+        public void PrimesSimple_PrimesNumbers_ReturnFalse(ulong value)
+        {
+            //arrange
+            bool isPrime;
+
+            //act
+            isPrime = Mathf.IsPrime(value);
+
+            //assert
+            Assert.IsFalse(isPrime);
+        }
+    }
+}
---
+++ UnitTestsPrimes/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestsPrimes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestsPrimes")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7a561f93-0aac-4388-abf5-d55a015fc7d3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
